1394cfcdc26d867f5cf8a300bf29b7b3f5c0eaf2
Oscar Ratcliffe <oscar@hackintosh.home>
2021-12-11 02:06:05 +0000

    Started working on frontend auth and created function to check if ID is valid aka in the db

--- Backend/main.go
+++ Backend/main.go
@@ -1,143 +1,197 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
-    "net"
-    "database/sql"
-    "strconv"
-    "fmt"
-    "strings"
-    "github.com/glendc/go-external-ip"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	externalip "github.com/glendc/go-external-ip"
+	_ "github.com/lib/pq"
 )
 
 // https://gobyexample.com/command-line-arguments
 const (
-    host = "172.17.0.2"
-    port = 5432
-    user = "postgres"
-    password = "Tillet";
-    dbname = "tillet_coin"
+	host     = "172.17.0.1"
+	port     = 5432
+	user     = "postgres"
+	password = "Tillet"
+	dbname   = "tillet_coin"
 )
-  
+
 // User represents a users data
 type userstruct struct {
-    ID string `json:"ID"`
-    FName string `json:"FName"`
-    LName string `json:"LName"`
-    Wealth int `json:"Wealth"`
-    Class1 string `json: Class1`
-    Class2 string `json: Class2`
-    Class3 string `json: Class3`
-    Class4 string `json: Class4`
-    IsTeacher bool `json: IsTeacher`
-
+	ID        string `json:"ID"`
+	FName     string `json:"FName"`
+	LName     string `json:"LName"`
+	Wealth    int    `json:"Wealth"`
+	Class1    string `json: Class1`
+	Class2    string `json: Class2`
+	Class3    string `json: Class3`
+	Class4    string `json: Class4`
+	IsTeacher bool   `json: IsTeacher`
 }
 
 func GetInteralIP(port string) string {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil { panic(err) }
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic(err)
+	}
 
-    defer conn.Close()
-    localAddr := conn.LocalAddr().(*net.UDPAddr)
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-    return localAddr.IP.String() + ":" + port
+	return localAddr.IP.String() + ":" + port
 }
 
 func GetExternalIP(port string) string {
-    consensus := externalip.DefaultConsensus(nil, nil)
-    ip, err := consensus.ExternalIP()
-    if err != nil {
-        panic(err)
-    }
+	consensus := externalip.DefaultConsensus(nil, nil)
+	ip, err := consensus.ExternalIP()
+	if err != nil {
+		panic(err)
+	}
 
-    return ip.String() + ":" + port
+	return ip.String() + ":" + port
 }
 
-// Gets user data from the database when given an ID 
-func GetUserData(ID string) [9]string{
-    var usrdata[9] string
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
+// Gets user data from the database when given an ID
+func GetUserData(ID string) [9]string {
+	var usrdata [9]string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil { panic(err) }
-    err = db.Ping()
-    if err != nil { panic(err) }
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
-    var sqlStatement string = `SELECT id, first_name, last_name, wealth, class1, class2, class3, class4, isteacher FROM users WHERE id=$1;`
-    var id string
-    var first_name string
-    var last_name string
-    var wealth int
-    var class1 sql.NullString
-    var class2 sql.NullString
-    var class3 sql.NullString
-    var class4 sql.NullString
-    var isteacher bool
-    row := db.QueryRow(sqlStatement, ID)
-    switch err := row.Scan(&id, &first_name, &last_name, &wealth, &class1, &class2, &class3, &class4, &isteacher); err {
-        case sql.ErrNoRows:
-            fmt.Println("No rows found")
-        case nil:
-            usrdata[0] = id
-            usrdata[1] = first_name
-            usrdata[2] = last_name
-            usrdata[3] = strconv.Itoa(wealth)
-            if class1.Valid {usrdata[4] = class1.String} else {usrdata[4] = "null"}
-            if class2.Valid {usrdata[5] = class1.String} else {usrdata[5] = "null"}
-            if class3.Valid {usrdata[6] = class1.String} else {usrdata[6] = "null"}
-            if class4.Valid {usrdata[7] = class1.String} else {usrdata[7] = "null"}
-            if isteacher {usrdata[8] = "true"} else {usrdata[8] = "false"}
-        
-        default:
-            panic(err)
-    }
-    return usrdata
+	var sqlStatement string = `SELECT id, first_name, last_name, wealth, class1, class2, class3, class4, isteacher FROM users WHERE id=$1;`
+	var id string
+	var first_name string
+	var last_name string
+	var wealth int
+	var class1 sql.NullString
+	var class2 sql.NullString
+	var class3 sql.NullString
+	var class4 sql.NullString
+	var isteacher bool
+	row := db.QueryRow(sqlStatement, ID)
+	switch err := row.Scan(&id, &first_name, &last_name, &wealth, &class1, &class2, &class3, &class4, &isteacher); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows found")
+	case nil:
+		usrdata[0] = id
+		usrdata[1] = first_name
+		usrdata[2] = last_name
+		usrdata[3] = strconv.Itoa(wealth)
+		if class1.Valid {
+			usrdata[4] = class1.String
+		} else {
+			usrdata[4] = "null"
+		}
+		if class2.Valid {
+			usrdata[5] = class1.String
+		} else {
+			usrdata[5] = "null"
+		}
+		if class3.Valid {
+			usrdata[6] = class1.String
+		} else {
+			usrdata[6] = "null"
+		}
+		if class4.Valid {
+			usrdata[7] = class1.String
+		} else {
+			usrdata[7] = "null"
+		}
+		if isteacher {
+			usrdata[8] = "true"
+		} else {
+			usrdata[8] = "false"
+		}
+
+	default:
+		panic(err)
+	}
+	return usrdata
 }
 
 func main() {
 	router := gin.Default()
 
-    //Get info for a user given their ID
-    router.GET("/user/:id", func(c *gin.Context) {
-        ID := c.Param("id")
-        UserData := GetUserData(ID)
-        Wealthint, err := strconv.Atoi(UserData[3])
-        if err != nil { panic(err) }
-            if UserData[8] == "false" {
-                c.IndentedJSON(http.StatusOK, []userstruct {
-                    {ID: UserData[0], FName: UserData[1], LName: UserData[2], Wealth: Wealthint, Class1: UserData[4],Class2: UserData[5],Class3: UserData[6],Class4: UserData[7], IsTeacher: false},
-                })} else {
-                c.IndentedJSON(http.StatusOK, []userstruct {
-                    {ID: UserData[0], FName: UserData[1], LName: UserData[2], Wealth: Wealthint, Class1: UserData[4],Class2: UserData[5],Class3: UserData[6],Class4: UserData[7], IsTeacher: true},
-                })
-            }
-    })
+	//Get info for a user given their ID
+	router.GET("/user/getdata/:id", func(c *gin.Context) {
+		ID := c.Param("id")
+		UserData := GetUserData(ID)
+		Wealthint, err := strconv.Atoi(UserData[3])
+		if err != nil {
+			panic(err)
+		}
+		if UserData[8] == "false" {
+			c.IndentedJSON(http.StatusOK, []userstruct{
+				{ID: UserData[0], FName: UserData[1], LName: UserData[2], Wealth: Wealthint, Class1: UserData[4], Class2: UserData[5], Class3: UserData[6], Class4: UserData[7], IsTeacher: false},
+			})
+		} else {
+			c.IndentedJSON(http.StatusOK, []userstruct{
+				{ID: UserData[0], FName: UserData[1], LName: UserData[2], Wealth: Wealthint, Class1: UserData[4], Class2: UserData[5], Class3: UserData[6], Class4: UserData[7], IsTeacher: true},
+			})
+		}
+	})
 
-    router.GET("/user", func(c *gin.Context) {
-        c.String(http.StatusOK, "You forget the ID")
-    })
-    fmt.Println("Starting...")
+	router.GET("/user/isvalid/:id", func(c *gin.Context) {
+		ID := c.Param("id")
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
 
-    var IsReserved string
-    var port string   
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			panic(err)
+		}
+		err = db.Ping()
+		if err != nil {
+			panic(err)
+		}
+		row := db.QueryRow(`SELECT ID FROM users WHERE id=$1;`, ID)
+		switch err := row.Scan(&ID); err {
+		case sql.ErrNoRows:
+			c.Status(http.StatusNoContent)
+		case nil:
+			c.Status(http.StatusOK)
+		}
+	})
 
-    fmt.Println("Do you want to use an internal or external address? (I/E)")
-    fmt.Scanln(&IsReserved)
+	router.GET("/user", func(c *gin.Context) {
+		c.String(http.StatusOK, "You forget the ID")
+	})
+	fmt.Println("Starting...")
 
-    fmt.Println("Please enter a port number (Deafult: 8080)")
-    fmt.Scanln(&port)
-    if port == "" { port = "8080" }
+	var IsReserved string
+	var port string
 
-    switch strings.ToUpper(IsReserved) {
-    case "I":
-        router.Run(GetInteralIP(port))
-    case "E":
-        router.Run(GetExternalIP(port))
-    default:
-        fmt.Println("Please enter I/E")
-    }
+	fmt.Println("Do you want to use an internal or external address? (I/E)")
+	fmt.Scanln(&IsReserved)
+
+	fmt.Println("Please enter a port number (Deafult: 8080)")
+	fmt.Scanln(&port)
+	if port == "" {
+		port = "8080"
+	}
+
+	switch strings.ToUpper(IsReserved) {
+	case "I":
+		router.Run(GetInteralIP(port))
+	case "E":
+		router.Run(GetExternalIP(port))
+	default:
+		fmt.Println("Please enter I/E")
+	}
 }
